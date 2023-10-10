09afed5bfca7af5af79e9ee80c526ef083c19dcd
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2023-10-10 21:32:27 +0800

    30 - Hit Flash Material & Driving Material parameters through C++
    -make a flash material by using c++ set material parameters when attributecomp event was trigger
    -by using material-instance,we can quickly adjust the effection of material

---
+++ Source/ActionRoguelike/Private/STargetDummy.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "STargetDummy.h"
+#include "Components/StaticMeshComponent.h"
+#include "SAttributeComponent.h"
+
+// Sets default values
+ASTargetDummy::ASTargetDummy()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComp");
+	RootComponent = MeshComp;
+
+	AttributeComp = CreateDefaultSubobject<USAttributeComponent>("AttributeComp");
+	AttributeComp->OnHealthChanged.AddDynamic(this, &ASTargetDummy::OnHealthChanged);
+}
+
+// Called when the game starts or when spawned
+void ASTargetDummy::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASTargetDummy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASTargetDummy::OnHealthChanged(AActor* InstigatorActor, USAttributeComponent* OwningComp, float NewHealth, float Delta)
+{
+	// 代表受到了伤害，展现一种材质，以后受到治疗可以展现其他样式的材质
+	if (Delta < 0.0f)
+	{
+		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->GetTimeSeconds());
+	}
+}
+
