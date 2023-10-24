5058bd7d01d253f0a7bcb3efe2e311479d5970bd
四月薰香满樱落 <70734031+syxxmyl@users.noreply.github.com>
2023-10-24 16:33:43 +0800

    103 - Animation State Machines (Stunned Effect) & C++ AnimInstancen
    -add an animation state about player stunned

---
+++ Source/ActionRoguelike/Public/SAnimInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SAnimInstance.generated.h"
+
+
+class USActionComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE_API USAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+protected:
+	void NativeInitializeAnimation() override;
+
+	void NativeUpdateAnimation(float DeltaSeconds) override;
+
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Animation")
+	bool bIsStunned;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Animation")
+	USActionComponent* ActionComp;
+};
